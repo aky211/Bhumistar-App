e905814bc78f8ab84b0e2f7df1d9e1d53e715f80
aky211 <amanbhaiyaofficial@gmail.com>
2022-01-25 16:00:44 +0800

    final

---
+++ android/app/src/main/kotlin/com/digitalaman/bhumistar_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.digitalaman.bhumistar_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Home.dart
@@ -0,0 +1,28 @@
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  void initState() {
+    super.initState();
+    // Enable virtual display.
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+        child: WebView(
+          initialUrl: 'https://bhumistar.com',
+        )
+    );
+  }
+}
